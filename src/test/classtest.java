459d484fd23fbae144b622acc5d3a41743b88bc2
lulit <lulit@DESKTOP-0EPS11B>
2021-03-18 14:48:34 +0100

    2eme push

--- src/test/classtest.java
+++ src/test/classtest.java
@@ -3,7 +3,7 @@ package test;
 public class classtest {
 
 	public static void main(String[] args) {
-		System.out.println("hello!");
+		System.out.println("hello Lulit!");
 		// TODO Auto-generated method stub
 
 	}
