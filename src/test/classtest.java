276e5a6374e93e4cfa35a32c6aa355ed7d7f38a7
lulit <lulit@DESKTOP-0EPS11B>
2021-03-18 14:52:26 +0100

    Revert "2eme push"
    This reverts commit 459d484fd23fbae144b622acc5d3a41743b88bc2.

--- src/test/classtest.java
+++ src/test/classtest.java
@@ -3,7 +3,7 @@ package test;
 public class classtest {
 
 	public static void main(String[] args) {
-		System.out.println("hello Lulit!");
+		System.out.println("hello!");
 		// TODO Auto-generated method stub
 
 	}
